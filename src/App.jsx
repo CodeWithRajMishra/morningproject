52b7e88945aecd4a9cdb3368db4cf84cac2aa86e
unknown <rajmishra3@gmail.com>
2024-06-24 14:51:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+const App=()=>{
+  return(
+    <>
+         <h1> Hello we are Cybrom Students</h1>
+
+         <div id="box1">
+            <div id="box2">
+               <div id="box3">         
+                    <div id="box4">
+                    CYBROM
+                     </div>
+               </div>
+            </div>
+         </div>
+
+    </>
+  )
+}
+export default App;
